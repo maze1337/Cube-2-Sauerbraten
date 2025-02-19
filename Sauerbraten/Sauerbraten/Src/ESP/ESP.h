7e23fe2e6e687a3df4518ccbbd8e88687c76ac51
Maze <78443372+maze1337@users.noreply.github.com>
2025-02-19 18:03:24 +0100

    Delete Sauerbraten directory

--- Sauerbraten/Sauerbraten/Src/ESP/ESP.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../Includes/Includes.h"
-
-namespace ESP
-{
-	void Box(Vector2 Bottom, Vector2 Top);
-	void HealthBar(Vector2 Bottom, Vector2 Top, int Health);
-	void Name(Vector2 Top, std::string Name);
-}
--- Sauerbraten/Sauerbraten/Src/Hack/Hack.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../Includes/Includes.h"
-
-namespace Hack
-{
-	void Run();
-	void Draw();
-}
